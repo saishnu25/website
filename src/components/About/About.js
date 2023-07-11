ba006e44df58f6759e1232536e0deda7d42f1daa
Saishnu Ramesh Kumar <saishnu2503@gmail.com>
2023-07-11 17:08:09 +0800

    test

---
+++ src/components/About/About.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+          <Image
+            src="https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/man-svgrepo-com.svg"
+            alt="man-svgrepo"
+          />
+          <div className="AboutBio">
+            Hello! My name is <strong>Your Name</strong> Lorem Ipsum is simply
+            dummy text of the printing and typesetting industry. Lorem Ipsum has
+            been the industry's standard dummy text ever since the 1500s, when
+            an unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essentially
+            unchanged.
+          </div>
+          <div className="AboutBio tagline2">
+            I have become confident using the following technologies.
+          </div>
+          <Technologies>
+            {stackList.map((stack, index) => (
+              <Tech key={index} className="tech">
+                <TechImg src={stack.img} alt={stack.name} />
+                <TechName>{stack.name}</TechName>
+              </Tech>
+            ))}
+          </Technologies>
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,74 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_1.png",
+    title: "Spense",
+    description: "Codewell challenge, Spense is a landing page for writers.",
+    tech_stack: "Html, Css, Javascript",
+    github_url: "https://your_project_github_link",
+    demo_url: "https://your_project_demo_link",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_2.png",
+    title: "Fiber",
+    description:
+      "An online portfolio generator. Great to practice flex/grid layouts, absolute positioning and sliders.",
+    tech_stack: "Html, Css, Javascript",
+    github_url: "https://your_project_github_link",
+    demo_url: "https://your_project_demo_link",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/portfolio.png",
+    title: "Portfolio",
+    description: "Ofcourse i made my own personal portfolio website.",
+    tech_stack: "Reactjs, Css",
+    github_url: "https://your_project_github_link",
+    demo_url: "https://your_project_demo_link/",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/project_3.png",
+    title: "Ecommerce website",
+    description:
+      "Created simple ecommerce website using reactjs and react-use-cart plugin.",
+    tech_stack: "Reactjs, Css",
+    github_url: "https://your_project_github_link",
+    demo_url: "https://your_project_demo_link",
+  },
+];
+
+// --------------------------------------- Skills
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/html5.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/css3.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/javascript.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/react.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/bootstrap.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/sass.svg",
+    name: "Scss",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/git.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/gurupawar/website/main/src/Assets/skill/responsive.svg",
+    name: "Responsive",
+  },
+];
